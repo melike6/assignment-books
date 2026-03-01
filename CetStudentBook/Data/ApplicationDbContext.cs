3f80e1d3455df8c2eae6fe4d1aedac63efae9f29
melike <111898457+melike6@users.noreply.github.com>
2026-03-02 01:32:41 +0300

    Book'u Veritabanına Bağlama

--- CetStudentBook/Data/ApplicationDbContext.cs
+++ CetStudentBook/Data/ApplicationDbContext.cs
@@ -1,16 +1,21 @@
-﻿using CetStudentBook.Models;
+﻿
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using CetStudentBook.Models;
 
 namespace CetStudentBook.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
 
+      
         public DbSet<Student> Students { get; set; }
 
-
-
-
+      
+        public DbSet<Book> Books { get; set; }
     }
 }
